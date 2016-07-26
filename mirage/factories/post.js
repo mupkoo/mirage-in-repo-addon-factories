12c962a5c2b8a74dfb90c5b0e69e1fed546d9679
Mirko Akov <mupkoo@gmail.com>
2016-07-26 17:34:46 +0300

    Add Post model

---
+++ mirage/factories/post.js
@@ -0,0 +1,11 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  title() {
+    return faker.lorem.sentence();
+  },
+
+  body() {
+    return faker.lorem.paragraphs();
+  }
+});
